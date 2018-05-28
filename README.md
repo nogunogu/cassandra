fa452e4822bde652ee03e787be92a0ef688ed05c
Koichi NOGUCHI <koichi.noguchi@rakuten.com>
2018-05-28 09:46:25 +0900

    You can execute a recipe, cassandra::config, to apply new configuration.

--- README.md
+++ README.md
@@ -2,3 +2,21 @@
 
 TODO: Enter the cookbook description here.
 
+DataCenterの追加。
+
+追加するDCのSeedノードを決める
+cassandra.yaml (SnitchとSeeds, auto_bootstrap: false)
+cassandra-rackdc.properties でDC, Rackを定義
+cassandra-topology.properties でDC, Rack, Nodeを定義
+
+新しいノードとDCを立ち上げ
+
+キースペースの更新
+ALTER KEYSPACE nogu WITH replication = {
+  'class': 'NetworkTopologyStrategy', 'DC1': 2, 'DC2': 2
+};
+
+nodetool rebuild -- DC1（DC2)　を全ノード上で実施（やり方、効果を要調査）
+
+nodetool repairしないとデータ同期されない？
+auto_bootstrapをtrueにして再起動すれば同期される？
--- recipes/default.rb
+++ recipes/default.rb
@@ -19,24 +19,4 @@ end
 
 package 'cassandra'
 
-template '/etc/cassandra/cassandra.yaml' do
-  source 'cassandra.yaml.erb'
-  notifies :run, 'execute[stop cassandra]', :immediately
-end
-
-execute 'stop cassandra' do
-  command 'service cassandra stop'
-  action :nothing
-  notifies :run, 'execute[remove system data]', :immediately
-end
-
-execute 'remove system data' do
-  command 'rm -rf /var/lib/cassandra/data/system/*'
-  action :nothing
-  notifies :run, 'execute[start cassandra]', :immediately
-end
-
-execute 'start cassandra' do
-  command 'service cassandra start'
-  action :nothing
-end
+include_recipe 'cassandra::config'
