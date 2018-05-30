15edb8092b2558cc56b1f1c3aa34171ac22524bd
Koichi NOGUCHI <koichi.noguchi@rakuten.com>
2018-05-30 15:13:28 +0900

    Refactor table part, and add new table & data.

--- recipes/table.rb
+++ recipes/table.rb
@@ -3,16 +3,31 @@
 # Recipe:: table
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-template '/tmp/simple_primary_key.cql' do
-  source 'simple_primary_key.erb'
+template '/tmp/create_table.cql' do
+  source 'table/create_table.erb'
 end
 
 # execute when keyspace is not exists
-execute 'cqlsh -f /tmp/simple_primary_key.cql'
+execute 'cqlsh -f /tmp/create_table.cql'
 
-template '/tmp/multiple_primary_key.cql' do
-  source 'multiple_primary_key.erb'
+template '/tmp/data_cyclist_category.csv' do
+  source 'table/data_cyclist_category.erb'
 end
 
+template '/tmp/data_cyclist_name.csv' do
+  source 'table/data_cyclist_name.erb'
+end
+
+template '/tmp/data_rank_by_year_and_name.csv' do
+  source 'table/data_rank_by_year_and_name.erb'
+end
+
+template '/tmp/data_videos_by_tag.csv' do
+  source 'table/data_videos_by_tag.erb'
+end
+
+template '/tmp/import_csv.cql' do
+  source 'table/import_csv.erb'
+end
 # execute when keyspace is not exists
-execute 'cqlsh -f /tmp/multiple_primary_key.cql'
+execute 'cqlsh -f /tmp/import_csv.cql'
